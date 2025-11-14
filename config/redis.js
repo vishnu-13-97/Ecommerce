f32d6935e2cc56a686665d70d7e7347828f3dc5e
Vishnu <vishnu.cp.13@gmail.com>
2025-11-14 12:27:36 +0530

    Removed old backend files; cleaned project structure

--- config/redis.js
+++
@@ -1,6 +0,0 @@
-const Redis = require('ioredis');
-const logger = require('./logger');
-
-const redis = new Redis(process.env.REDIS_HOST);
-logger.info('redis connected')
-module.exports = redis;
--- middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-
-const logger = require("../config/logger")
-
-module.exports = (err, req, res, next) => {
-  logger.error(err); 
-  res.status(500).json({ message: 'Internal Server Error' });
-};
