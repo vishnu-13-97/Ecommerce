c25844934445f9c14a94b19485802522212a7340
Vishnu <vishnu.cp.13@gmail.com>
2026-03-05 09:39:56 +0530

    fix build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import "./assets/js/main.js";
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
